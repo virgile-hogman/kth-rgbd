514f200a0545fe547d5b4d17a7413787b0dcc8eb
virgile.hogman <virgile.hogman@gmail.com>
2011-06-08 15:19:07 +0000

    recreate src dirs from scratch

---
+++ CommonTypes.h
@@ -0,0 +1,21 @@
+
+#ifndef TYPES_H
+#define TYPES_H
+
+// -----------------------------------------------------------------------------------------------------
+//  common data types
+// -----------------------------------------------------------------------------------------------------
+typedef	unsigned short	TDepthPixel;	// 16 bits
+
+#define RGB_PIXELS_WIDTH	640
+#define RGB_PIXELS_HEIGHT	480
+
+#define DEPTH_PIXELS_WIDTH	640
+#define DEPTH_PIXELS_HEIGHT	480
+
+#define NBPIXELS_X_HALF 320
+#define NBPIXELS_Y_HALF 240
+
+#define MIDDLE_POINT	(640*240 + 320)	// just for debug
+
+#endif
