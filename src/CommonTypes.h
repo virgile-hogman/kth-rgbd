4278ee2dd1b046df8efd4f798c2b8b897e6e1f0b
virgile.hogman <virgile.hogman@gmail.com>
2011-08-17 14:38:13 +0000

    moved sources to folder src

---
+++ src/CommonTypes.h
@@ -0,0 +1,56 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <Eigen/Geometry>
+#include <Eigen/StdVector>
+#include <vector>
+
+// -----------------------------------------------------------------------------------------------------
+//  common data types
+// -----------------------------------------------------------------------------------------------------
+typedef	unsigned short	TDepthPixel;	// 16 bits
+
+#define NBPIXELS_WIDTH	640
+#define NBPIXELS_HEIGHT	480
+
+#define NBPIXELS_X_HALF 320
+#define NBPIXELS_Y_HALF 240
+
+#define MIDDLE_POINT	(640*240 + 320)	// just for debug
+
+// -----------------------------------------------------------------------------------------------------
+//  Transformations
+// -----------------------------------------------------------------------------------------------------
+class Transformation
+{
+public:
+	Eigen::Matrix4f	_matrix;
+	double			_error;
+	int				_idOrig;
+	int				_idDest;
+	float			_ratioInliers;
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+// for alignment read http://eigen.tuxfamily.org/dox/UnalignedArrayAssert.html
+};
+
+typedef std::vector<Transformation, Eigen::aligned_allocator<Eigen::Vector4f> > TransformationVector;
+
+
+class Pose
+{
+public:
+	Eigen::Matrix4f	_matrix;
+	int				_id;
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+// for alignment read http://eigen.tuxfamily.org/dox/UnalignedArrayAssert.html
+};
+
+typedef std::vector<Pose, Eigen::aligned_allocator<Eigen::Vector4f> > PoseVector;
+
+
+//vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Vector4f> > g_transformations;
+
+
+#endif
---
+++ src/rgbd_slam.cpp
@@ -0,0 +1,200 @@
+#include <boost/filesystem.hpp>
+
+#include "Config.h"
+#include "CameraDevice.h"
+#include "FrameData.h"
+#include "Map.h"
+
+// standard
+#include <iostream>
+#include <stdio.h>
+
+#include <vector>
+#include <list>
+
+using namespace std;
+
+// -----------------------------------------------------------------------------------------------------
+//  loadSequence
+// -----------------------------------------------------------------------------------------------------
+void loadSequence(const char *dataDirectory, int min, int max, vector<int> &sequenceFramesID)
+{
+	int frameID;
+	list<int> listFramesID;
+	
+	sequenceFramesID.clear();
+	
+	boost::filesystem::directory_iterator end_itr; // default construction yields past-the-end
+	for ( boost::filesystem::directory_iterator itr( dataDirectory );
+		itr != end_itr;
+		++itr )
+	{
+		//	printf("%s\t%s\n", itr->leaf().c_str(), itr->path().string().c_str());
+		if (boost::filesystem::extension(*itr)==".bmp" &&
+			sscanf(itr->leaf().c_str(), "frame_%d", &frameID)==1 && frameID>=min && (frameID<=max || max<0))
+		{
+			// add frame
+			//printf("Add frame file #%i:\t%s\n", frameID, itr->path().string().c_str());
+			listFramesID.push_back(frameID);
+		}
+	}
+	// sort and keep only 1 element (remove duplicates because of rgb+depth)
+	listFramesID.sort();
+	listFramesID.unique();
+	
+	// build the sequence
+	while (!listFramesID.empty())
+	{
+		sequenceFramesID.push_back(listFramesID.front());
+		listFramesID.pop_front();
+	}
+
+	cout << "Sequence of " << sequenceFramesID.size() << " frames available.\n";
+	cout << "Sequence of " << sequenceFramesID.size()/Config::_MatchingRatioFrame << " frames with ";
+	cout << "Frame ratio:" << Config::_MatchingRatioFrame << "\n";
+}
+
+void recordSequence(Map &map)
+{
+	CameraDevice cameraKinect;
+
+    if (cameraKinect.connect())
+    {
+    	int frameID = 0;
+    	Transformation transform;
+
+        boost::filesystem::create_directories(Config::_ResultDirectory);
+
+    	// generate 1st frame
+        if (cameraKinect.generateFrame(frameID))
+        {
+			frameID++;
+
+			map.initSequence();
+
+        	// generate new frame
+			while (cameraKinect.generateFrame(frameID))
+			{
+				// associate with previous
+				if (! map.addFrames(frameID-1, frameID, transform))
+				{
+					printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+					printf("!!! LOW QUALITY !!! LOST SYNCHRO !!!\n");
+					printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+					printf("Last Frame (%d) rejected\n", frameID);
+				}
+				else
+					frameID++;
+			}
+
+	    	// build map
+			if (!cameraKinect.aborted())
+				map.build();
+			else
+				printf("Aborted.\n");
+        }
+
+    	cameraKinect.disconnect();
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------
+//  Main program
+// -----------------------------------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+	vector<int> sequenceFramesID;
+	Map map;
+    
+	if (argc<1)
+	{
+		printf("Usage: %s -record | -match <from> <to> | -map <from> <to> | -pcd", argv[0]);
+		return -1;
+	}
+	
+	// load configuration
+	Config::LoadConfig("kth-rgbd.cfg");
+    FrameData::_DataPath = Config::_DataDirectory;
+
+    // ---------------------------------------------------------------------------------------------------
+    //  regenerate PCD files from archive
+    // ---------------------------------------------------------------------------------------------------
+    if (strcmp(argv[1], "-pcd") == 0)
+    {
+        printf("-Regenerate PCD files from graph archive-\n");
+
+    	if ( ! boost::filesystem::exists( Config::_DataDirectory ) )
+    		return -1;
+    	if ( ! boost::filesystem::exists( Config::_ResultDirectory ) )
+    		return -1;
+
+    	map.regeneratePCD();
+    }
+    // ---------------------------------------------------------------------------------------------------
+    //  map reconstruction from transformations archive
+    // ---------------------------------------------------------------------------------------------------
+    else if (strcmp(argv[1], "-map") == 0)
+    {
+        printf("-Reconstruct map from transformations archive, with loop closure-\n");
+
+    	if ( ! boost::filesystem::exists( Config::_DataDirectory ) )
+    		return -1;
+    	if ( ! boost::filesystem::exists( Config::_ResultDirectory ) )
+    		return -1;
+
+    	int min=-1, max=-1;
+    	if (argc>3)
+    		max = atoi(argv[3]);
+    	if (argc>2)
+    		min = atoi(argv[2]);
+
+    	map.restoreSequence(min, max);
+    	map.build();
+    }
+    // ---------------------------------------------------------------------------------------------------
+    //  load sequence from RGB+D input datafiles
+    // ---------------------------------------------------------------------------------------------------
+    else if (strcmp(argv[1], "-match") == 0)
+    {
+        printf("-Match and map a sequence of frames-\n");
+    	if ( ! boost::filesystem::exists( Config::_DataDirectory ) )
+    		return -1;
+
+    	int min=-1, max=-1;
+    	if (argc>3)
+    		max = atoi(argv[3]);
+    	if (argc>2)
+    		min = atoi(argv[2]);
+    	
+     	loadSequence(Config::_DataDirectory.c_str(), min, max, sequenceFramesID);
+    	
+        boost::filesystem::create_directories(Config::_ResultDirectory);       
+		
+    	// build map 
+        map.initSequence();
+    	map.addSequence(sequenceFramesID);
+    	map.build();
+	}
+    // ---------------------------------------------------------------------------------------------------
+    //  acquire data from camera and build map
+    // ---------------------------------------------------------------------------------------------------
+    else if (strcmp(argv[1], "-record") == 0)
+    {
+        printf("-Record sequence from camera-\n");
+        
+        // use the same directory to generate and reload the data
+        Config::_DataDirectory = Config::_GenDirectory;
+        FrameData::_DataPath = Config::_GenDirectory;
+    		
+        boost::filesystem::create_directories(Config::_DataDirectory);
+        boost::filesystem::create_directories(Config::_ResultDirectory);
+
+        recordSequence(map);
+    }
+    else {
+		printf("Usage: %s -record | -match <from> <to> | -map <from> <to> | -pcd", argv[0]);
+    	return -1;
+    }
+    
+    return 0;
+}
